ac54df6f20a1b901b4471f57adeb099c760bbda0
dZatr1k <uhuman239@gmail.com>
2023-04-19 00:50:49 +0300

    added ps and some fixes

--- TowerDefense/Assets/Scripts/Heroes/KamikazeWizard.cs
+++ TowerDefense/Assets/Scripts/Heroes/KamikazeWizard.cs
@@ -5,19 +5,14 @@ public class KamikazeWizard : Hero
 {
     [SerializeField] private BoxCollider2D _attackZoneCollider;
     [SerializeField] private float _delayBeforeBam = 0.5f;
-    private bool _canComplate = false;
+    
+    private ParticleSystem _particleSystem;
 
-    private void OnCollisionStay2D(Collision2D collision) { }
-
-    private void OnTriggerEnter2D(Collider2D other)
+    private void Start()
     {
+        _particleSystem = GetComponentInChildren<ParticleSystem>();
+
         StartCoroutine(WaitForSeconds(_delayBeforeBam));
-        if (_canComplate)
-        {
-            Transform attackZoneTransform = _attackZoneCollider.gameObject.transform;
-            Collider2D[] hitColliders = Physics2D.OverlapBoxAll(attackZoneTransform.position, _attackZoneCollider.size, 0, layerMask: LayerMask.GetMask("Default"));
-            Attack(hitColliders);
-        }
     }
 
     protected void Attack(Collider2D[] hitColliders)
@@ -32,13 +27,19 @@ public class KamikazeWizard : Hero
                 enemy.TakeDamage(Damage);
             }
         }
-        if(IsEnemy)
+        if (IsEnemy) 
+        {
+            _particleSystem.Play();
             StartCoroutine(Die());
+        }
     }
 
     protected override IEnumerator WaitForSeconds(float time)
     {
         yield return new WaitForSeconds(time);
         _canComplate = true;
+        Transform attackZoneTransform = _attackZoneCollider.gameObject.transform;
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(attackZoneTransform.position, _attackZoneCollider.size, 0, layerMask: LayerMask.GetMask("Default"));
+        Attack(hitColliders);
     }
 }
